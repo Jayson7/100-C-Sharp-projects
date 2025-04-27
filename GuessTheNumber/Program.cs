b57c25de0fc653df0bd5342c4be49ce0d06890ae
jayson7 <lexxiijoo70@gmail.com>
2025-04-27 04:52:34 +0100

    GuessTheNumber

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the Number Game!\n");
+            Console.WriteLine("I'm thinking of a number between 1 and 100.\n");
+
+            Random random = new Random();
+            int targetNumber = random.Next(1, 101);
+            int guess = 0;
+            int attempts = 0;
+
+            while (guess != targetNumber)
+            {
+                Console.Write("Enter your guess: ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    continue;
+                }
+
+                attempts++;
+
+                if (guess < targetNumber)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else if (guess > targetNumber)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine($"\nCongratulations! You guessed it in {attempts} attempt(s)!");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
